7cc934884906e643ae968f27418960de29a8042c
AnushkaBhushan <75431213+AnushkaBhushan@users.noreply.github.com>
2021-01-16 18:53:48 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+
+
+  class Block{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.3,
+            'friction':0.2,
+            'density':1.0
+        }
+        this.width = width;
+        this.height = height;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        //this.image = loadImage("sprites/base.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0, this.width,this.height);
+        pop();
+      }
+}
